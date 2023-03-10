ec9138170e13109ae9557ed6b9b5599c6ee35053
Neto Costa <netocjp@gmail.com>
2023-03-09 22:18:53 -0300

    Adicionando middleware de CORS

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 const express = require('express')
+const cors = require('cors')
 
 const routes = require('./routes/routes')
 
 const app = express()
+
+// habilita cors
+// libera para todos os servidores
+app.use(cors())
+
+// libera para um servidor especifico
+// app.use(cors({
+//   origin: 'http://127.0.0.1:5500'
+// })) 
+
+// libera para varios servidores
+const allowedOrigins = [
+  'http://127.0.0.1:5500',
+  'http://www.app.com.br'
+]
+
+app.use(cors({
+  origin: function (origin, callback) {
+    let allowed = true
+
+    if (!origin) allowed = true // mobile app
+
+    if (!allowedOrigins.includes(origin)) allowed = false
+
+    callback(null, allowed)
+  }
+}))
 
 // receber dados do body como json
 app.use(express.json())
